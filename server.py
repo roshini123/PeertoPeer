d9a3f637db12b7af39ec452535d0f815ddfbd1e3
rravill <rravill@ncsu.edu>
2016-03-31 14:06:18 -0700

    Added threading

--- server.py
+++ server.py
@@ -16,6 +16,13 @@ rfc_list = []
                 self.title=title
                 self.hostName=hostName
 
+ class PeerThread(threading.Thread):
+        def __init__(self, client, addr):
+                threading.Thread.__init__(self)
+                self.client=client
+                self.addr=addr
+		
+
  if __name__ == "__main__"
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         host = socket.gethostname()
@@ -24,5 +31,6 @@ rfc_list = []
         s.listen(10)
         while True:
                 c,addr = s.accept()
-	
+		peer=PeerThread(c, addr)
+                peer.start()	
 	s.close()
