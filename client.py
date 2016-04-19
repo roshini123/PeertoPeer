f0cb27ebecda668c8616ffa9a5933ca0a0876e26
rravill <rravill@ncsu.edu>
2016-04-19 14:28:02 -0700

    client-to-server added

--- client.py
+++ client.py
@@ -3,11 +3,91 @@ import os
 import time
 import platform
 import threading
+import shlex
 
+class client_to_serv_comm(threading.Thread):
+	def __init__(self,upload_port,download_port,server_ip,client_to_server,ctos_name):
+		threading.Thread.__init__(self)
+		self.upload_port=upload_port
+		self.download_port=download_port
+		self.server_ip=server_ip
+		self.client_to_server=client_to_server
+		self.ctos_name=ctos_name
+
+	#	self.start()
+	
+	def run(self):
+		self.client_to_server.connect((self.server_ip,self.download_port))
+		while True:
+			print("\n1.Add RFC\n2.LOOKUP RFC\n3.LISTALL RFCs\n4.Exit\n Enter your choice")
+			choice=int(input())	
+			if choice==1:
+				print("Enter the RFC number:")
+				rfcnumber=str(raw_input())
+				print("Enter the title of the RFC")
+				rfctitle=str(raw_input())
+				add_request='ADD RFC '+rfcnumber+' P2P-CI/1.0\nHost: '+str(self.ctos_name)+'\nPort: '+str(self.upload_port)+'\nTitle: '+rfctitle+'\n'
+				self.client_to_server.send(add_request)
+				add_reply=self.client_to_server.recv(2048)
+				print("\n"+add_reply+"\n")
+			elif choice==2:
+				print("Enter the RFC number to lookup:")
+				rfcnumber=str(raw_input())
+				print("Enter the RFC title: ")
+				rfctitle=str(raw_input())
+				lookup_request='LOOKUP RFC '+rfcnumber+' P2P-CI/1.0\nHost: '+self.ctos_name+'\nPort: '+str(self.upload_port)+'\nTitle: '+rfctitle+'\n'
+				self.client_to_server.send(lookup_request)
+				lookup_reply=self.client_to_server.recv(2048)
+
+				if '200 OK' in lookup_reply:
+					lines=lookup_reply.split("\n")
+					for i in range(1,len(lines)-1):
+						print(lines[i]+"\n")
+					print("press 1 to download :")
+					val=int(input())
+					if val==1:
+						print("enter the host name:")
+						host=str(raw_input())
+						print("enter the download port:")
+						port=int(input())
+						download_request='GET RFC '+rfcnumber+' P2P-CI/1.0\nHost: '+str(self.ctos_name)+'\nOS: '+platform.platform()+'\n'
+						new_rfc_sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+						new_rfc_sock.connect((host,port))
+						new_rfc_sock.send(download_request)
+						filnam='recvfiles/RFC'+rfcnumber+'.txt'
+						data=''
+						with open(filnam,'wb') as fs:
+        						data+=new_rfc_sock.recv(1024)
+        						s,a=data.split("Content Type: text/text\n")
+        						fs.write(a)
+						fs.close()
+						new_rfc_sock.close()
+						
+				else:
+					print(lookup_reply)
+			elif choice==3:
+				list_request='LIST ALL P2P-CI/1.0\nHost: '+self.ctos_name+'\nPort: '+str(self.upload_port)+'\n'
+				self.client_to_server.send(list_request)
+				list_reply=self.client_to_server.recv(4098)
+				if '200 OK' in list_reply:
+					lines=list_reply.split("\n")
+					for i in range(1,len(lines)-1):
+						print(lines[i]+"\n")
+				else:
+					print(list_reply)
+			elif choice==4:
+				self.client_to_server.close()
+				break
+			else:
+				print("wrong choice")
+
+
+	
 class client_to_client_comm(threading.Thread):
 	def __init__(self,client_to_client,ctoc_name):
 		self.client_to_client=client_to_client
 		self.ctoc_name=ctoc_name 
+		threading.Thread.__init__(self)
 
 	def run(self):
 		c,addr = self.client_to_client.accept()
@@ -18,6 +98,7 @@ class peer_to_download(threading.Thread):
 	def __init__(self,peer_obj, peer_addr):
 		self.peer_obj=peer_obj
 		self.peer_addr=peer_addr
+		threading.Thread.__init__(self)
 
 	def run(self):
 		message=self.peer_obj.recv(2048)
@@ -30,27 +111,34 @@ class peer_to_download(threading.Thread):
 			else:
 				reply_message='P2P-CI/1.0 200 OK\n'+'Date: '+time.asctime()+'\nOS: '+platform.platform()+'\nLast Modified:\nContent Length:\nContent Type: text/text\n'
 				filename='RFCList/'+datasplit[1]+datasplit[2]+'.txt'
-				f=open(filename,'rb')
-				l=f.read(1024)
-				while(l):
-					reply_message+=l
+				try:
+					f=open(filename,'rb')
 					l=f.read(1024)
-				f.close()
+					while(l):
+						reply_message+=l
+						l=f.read(1024)
+					f.close()
+				except:
+					reply_file='////the content is empty as the requested file does not exist on the requested peer'
 				self.peer_obj.send(reply_message)
+		#		self.peer_obj.send(reply_file)
 				self.peer_obj.close()
-
-if __name__="__main__":
-	download_port=7735
-	upload_port=int(input("enter the upload port number: ")
-	server_ip=input("enter the ip-address of the server host:")
+				
+if __name__=="__main__":
+	download_port=7745
+	server_ip='127.0.0.1'
+	print("enter the upload port number: ")
+	upload_port=int(input())
+	print("enter the ip-address of the host:")
+	ctos_name=str(raw_input())
 	client_to_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    	ctos_name = socket.gethostname()
-	client_to_serv_comm(upload_port,download_port,server_ip,client_to_server,ctos_name)
-	
-
+	print ctos_name+" is the host name"
+	c=client_to_serv_comm(upload_port,download_port,server_ip,client_to_server,ctos_name)
+	c.start()
 	client_to_client=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	ctoc_name = socket.gethostname()
-	client_to_client.bind ((ctos_name,upload_port))
+	ctoc_name = ctos_name
+
+	client_to_client.bind ((ctoc_name,upload_port))
         client_to_client.listen(10)
 	ctoc=client_to_client_comm(client_to_client,ctoc_name)
 	ctoc.start()
--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 import socket
 import threading
 import os
+import shlex
 
 peers = []
 rfcs = []
-global counter
+counter=0
+rfclock=threading.Lock()
 
 class PeerList:
         def __init__(self,hostName="None",port=11111):
@@ -15,7 +17,7 @@ class PeerList:
 			return self.port
 		return "None"
 class RFCList:
-        def __init__(self,rfcNumber,title="None", hostName="None"):
+        def __init__(self,rfcNumber,title, hostName):
                 self.rfcNumber=rfcNumber
                 self.title=title
                 self.hostName=hostName
@@ -40,59 +42,60 @@ class PeerThread(threading.Thread):
 	  con=0
 	  while(con==0):
 		message=self.client.recv(2048)
-	    	
+		print(message+'\n')	    	
 		if(len(message)!=0):
 			datasplit=shlex.split(message)
-			global counter 
+			global counter
 			index=0
 			if self.count==0:
 				self.count=self.count+1
-				self.sethostName(datasplit[5])
-				peers.insert(0,peerList(datasplit[5],int(datasplit[7])))
+				if datasplit[0]=='LIST':
+					self.sethostName(datasplit[4])
+					peers.insert(0,PeerList(datasplit[4],int(datasplit[6])))
+				else:
+					self.sethostName(datasplit[5])
+					peers.insert(0,PeerList(datasplit[5],int(datasplit[7])))
 				counter=counter+1
 	
 			req=datasplit[0]
 			if req=="ADD":
-				titlesplit=message.rsplit('Title:',1)
-				title=titlesplit[1].rsplit('ADD',1)
-				rfcs.insert(0,RFCList(int(datasplit[2]), title[0], datasplit[5]))
-
-				index=message.find('ADD',index+3,len(message))
-				while(index!=-1):
-					tempsplit=shlex.split(title[1])
-					titlesplit=title[1].rsplit('Title:',1)
-        	                	title=titlesplit[1].rsplit('ADD',1)
-					rfcs.insert(0,RFCList(int(tempsplit[1]), title[0],tempsplit[4]))
-					index=message.find('ADD',index+3,len(message))
-
+				titlesplit=message.rsplit('Title: ',1)
+				titletemp=titlesplit[1].rsplit('ADD',1)
+				title=titletemp[0].split("\n")
+				print("1."+str(datasplit[2])+"2."+title[0]+"3."+datasplit[5])
+				rfclock.acquire()
+				rfcs.insert(0,RFCList(datasplit[2], title[0], datasplit[5]))
+				rfclock.release()
+				response="P2P-CI/1.0 200 OK"+"\n"	
+				self.client.send(response)
 			elif req=="LOOKUP":
 				hcount=0
 				response="P2P-CI/1.0 200 OK"+"\n"
-				rfcnumber=int(datasplit[2])
+				rfcnumber=str(datasplit[2])
 				for x in range(0,len(rfcs)):
 					host=rfcs[x].getNameofHost(rfcnumber)
 					if(host!="None"):
 						hcount=hcount+1
 						for y in range(0,len(peers)):
 							port=peers[y].getPort(host)
-							response=response+"RFC "+rfcnumber+" "+rfcs[x].title+" "+host+" "+port+"\n"
-							break
+							if(port!="None"):
+								response=response+"RFC "+rfcnumber+" "+rfcs[x].title+" "+host+" "+str(port)+"\n"
+								break
 				if hcount==0:
 					response="P2P-CI/1.0 404 Not Found"
 				self.client.send(response)
 	
 			elif req=="LIST":
 				response="P2P-CI/1.0 200 OK"+"\n"
-				host=datasplit[4]
-				port=datasplit[6]
 				for x in range(0,len(rfcs)):
-					if rfcs[x].hostName==host:
-						response=response+"RFC "+rfcs[x].getrfcNumber+" "+rfcs[x].title	+" "+host+" "+port+"\n"	
-						break
+					for y in range(0,len(peers)):
+						if(rfcs[x].hostName==peers[y].hostName):
+							response=response+"RFC "+rfcs[x].rfcNumber+" "+rfcs[x].title	+" "+rfcs[x].hostName+" "+str(peers[y].port)+"\n"	
+				self.client.send(response)
 			else:
 				response="P2P-CI/1.0 400 Bad Request"
 				self.client.send(response) 
-		else:
+	  else:
 			con=1
 
 	  for x in range(0,len(peers)):
@@ -103,13 +106,12 @@ class PeerThread(threading.Thread):
 		if rfcs[x].hostName==self.hostName:
 			rfcs.remove(rfcs[x])
 		
-
 	  self.client.close()		
 					 
 	
 if __name__ == "__main__":
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        host = socket.gethostname()
+        host = '127.0.0.1'
         port = 7745
         s.bind ((host,port))
         s.listen(10)
